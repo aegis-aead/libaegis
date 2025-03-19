97bc962dcaf91d6ce5016474daa0e10ef03e2809
Frank Denis <github@pureftpd.org>
2025-03-19 15:10:18 +0100

    Add

--- src/common/common.h
+++ src/common/common.h
@@ -45,7 +45,7 @@ static int errno;
 #    endif
 
 /* target pragmas don't define these flags on clang-cl (an alternative clang driver for Windows) */
-#    if defined(__clang__) && defined(_MSC_VER) && !defined(__MMX__)
+#    if defined(__clang__) && defined(_MSC_VER) && !defined(__SSE4_1__)
 #        define __MMX__     1
 #        define __SSE__     1
 #        define __SSE3__    1
